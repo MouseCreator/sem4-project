f110fb297fc5ed1e853f23cda261a095a0a8a6c5
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-17 02:12:26 +0200

    Added main model classes

---
+++ java/ModelApp/app/src/main/java/vtm/team/modelsapp/model/main/Model.java
@@ -0,0 +1,10 @@
+package vtm.team.modelsapp.model.main;
+
+import java.util.Set;
+
+public class Model {
+    private String name;
+    private String description;
+    private Set<Variable> variables;
+    private Set<User> contributors;
+}
---
+++ java/ModelApp/app/src/main/java/vtm/team/modelsapp/model/main/User.java
@@ -0,0 +1,7 @@
+package vtm.team.modelsapp.model.main;
+
+public class User {
+    private int id;
+    private String name;
+    private String info;
+}
