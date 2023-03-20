83385415df500c650c8c2c144b3c2f41033e2f93
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-03-20 15:17:55 +0200

    charts update

--- cpp/MODELIUM_2_0/variable.h
+++ cpp/MODELIUM_2_0/variable.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
@@ -6,6 +7,16 @@ class Variable
 {
 public:
     Variable();
+    Variable(std::string name, bool is_const = false, double default_value = 1.0){
+        this->name = name;
+        this->is_const = is_const;
+        this->default_value = default_value;
+        this->value = default_value;
+    };
+    std::string name;
+    bool is_const;
+    double value;
+    double default_value;
 };
 
 #endif // VARIABLE_H
