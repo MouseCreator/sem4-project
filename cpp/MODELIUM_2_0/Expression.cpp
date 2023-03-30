d2e042bd8a0f42e2aaade82525ac86f624784734
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-03-30 20:52:04 +0300

    error_box added
    this idea does NOT work , temporary solution

--- cpp/MODELIUM_2_0/Expression.cpp
+++ cpp/MODELIUM_2_0/Expression.cpp
@@ -30,11 +30,11 @@ namespace expr
         catch (std::runtime_error& e) {
             //std::cout << "Runtime error occured:\n " << e.what() << std::endl;
             //throw std::runtime_error(" ");
-            QMessageBox error_box;
-            error_box.setText("Invalid input (missing/extra brackets or invalid symbols\n");
-            error_box.setIcon(QMessageBox::Critical);
-            error_box.setWindowTitle("Input Error");
-            error_box.exec();
+            QMessageBox* error_box = new QMessageBox;
+            error_box->setText("Invalid input (missing or extra brackets or invalid symbols\n");
+            error_box->setIcon(QMessageBox::Critical);
+            error_box->setWindowTitle("Input Error");
+            error_box->exec();
         }
     }
 
@@ -230,16 +230,14 @@ namespace expr
     {
         if (!node_1 && !node_2)
             return true;
-        else if (!node_1 && node_2 || node_1 && !node_2)
+        else if ((!node_1 && node_2) || (node_1 && !node_2))
             return false;
         else
-            if (node_1->data == node_2->data ||
-                isNumber(node_1->data) && isNumber(node_2->data) &&
-                std::stod(node_1->data) == std::stod(node_2->data))
+            if (node_1->data == node_2->data || (isNumber(node_1->data) && isNumber(node_2->data) && std::stod(node_1->data) == std::stod(node_2->data)))
             {
                 if (node_1->data == "+" || node_1->data == "*")
-                    return (Compare(node_1->left, node_2->left) && Compare(node_1->right, node_2->right) ||
-                        Compare(node_1->left, node_2->right) && Compare(node_1->right, node_2->left));
+                    return ((Compare(node_1->left, node_2->left) && Compare(node_1->right, node_2->right)) ||
+                        (Compare(node_1->left, node_2->right) && Compare(node_1->right, node_2->left)));
                 else
                     if (isUnaryFunction(node_1->data)) {
                         return Compare(node_1->right, node_2->right);
@@ -441,7 +439,7 @@ namespace expr
             return pow(arg_1, arg_2);
         else if (function == "ln") {
             if (arg_1 <= 0)
-                throw std::overflow_error("Math error: Wrong function argument 'ln(a)', 'a' is less than or equal to Zorro\n");
+                throw std::overflow_error("Math error: Wrong function argument 'ln(a)', 'a' is less than or equal to Zero\n");
 
             return log(arg_1);
         }
@@ -479,7 +477,12 @@ namespace expr
             }
         }
         catch (std::overflow_error& e) {
-            std::cout << "Overflow error occurred\n" << e.what();
+            //std::cout << "Overflow error occurred\n" << e.what();
+            QMessageBox* error_box = new QMessageBox;
+            error_box->setText("Overflow error occurred\n");
+            error_box->setIcon(QMessageBox::Critical);
+            error_box->setWindowTitle("Input Error");
+            error_box->exec();
         }
     }
 
--- cpp/MODELIUM_2_0/mainwindow.cpp
+++ cpp/MODELIUM_2_0/mainwindow.cpp
@@ -18,7 +18,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     SavedModel* md = new SavedModel;
-    md->add_formula(Formula("x+y+z", "formula 1"));
+    md->add_formula(Formula("x/(1-1)", "formula 1"));
     md->add_formula(Formula("2*m/g", "formula 2"));
     md->add_formula(Formula("v*t", "formula 3"));
     md->add_formula(Formula("x*y*z*a*b*c*d*e*f*g*h*j*k*l*m*u*i", "formula 4"));
